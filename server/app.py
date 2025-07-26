e2b36d99c7b53feb3846a8e83878dd0517fc7452
Anthony Kuang <anthonykuang@berkeley.edu>
2025-07-26 22:56:53 +0800

    works

--- server/app.py
+++ server/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return "awWelcome to the Flask app!"
+
+@app.route("/capitalize", methods=["POST"])
+def capitalize_text():
+    data = request.get_json()
+    text = data.get("text", "")
+    capitalized = text.upper()
+    return jsonify({"result": capitalized})
